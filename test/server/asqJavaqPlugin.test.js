d132c4e8a4f79328fd712440e1ad4e0fb39152ff
Kyriakos Stylianopoulos <usi@usis-mbp.mobile.usilu.net>
2016-11-30 10:08:21 +0100

    renamed test file, code and html fixtures

--- test/server/asqJavaqPlugin.test.js
+++ test/server/asqJavaqPlugin.test.js
@@ -6,11 +6,11 @@ var should = chai.should();
 var expect = chai.expect;
 var cheerio = require('cheerio');
 var Promise = require('bluebird');
-var modulePath = "../../lib/asqMcPlugin";
+var modulePath = "../../lib/asqJavaqPlugin";
 var fs = require("fs");
 
-describe("asqMcPlugin.js", function(){
-  
+describe("asqJavaqPlugin.js", function(){
+
   before(function(){
     var then =  this.then = function(cb){
       return cb();
@@ -20,7 +20,7 @@ describe("asqMcPlugin.js", function(){
       then: then
     });
 
-    this.tagName = "asq-multi-choice-q";
+    this.tagName = "asq-java-q";
 
     this.asq = {
       registerHook: function(){},
@@ -37,30 +37,30 @@ describe("asqMcPlugin.js", function(){
     this.simpleHtml = fs.readFileSync(require.resolve('./fixtures/simple.html'), 'utf-8');
     this.noStemHtml = fs.readFileSync(require.resolve('./fixtures/no-stem.html'), 'utf-8');
     this.optionsHtml = fs.readFileSync(require.resolve('./fixtures/options.html'), 'utf-8');
-    
-    this.asqMcPlugin = require(modulePath);
+
+    this.asqJavaqPlugin = require(modulePath);
   });
 
   describe("parseHtml", function(){
 
     before(function(){
-     sinon.stub(this.asqMcPlugin.prototype, "processEl").returns("res");
+     sinon.stub(this.asqJavaqPlugin.prototype, "processEl").returns("res");
     });
 
     beforeEach(function(){
-      this.asqmc = new this.asqMcPlugin(this.asq);
-      this.asqMcPlugin.prototype.processEl.reset();
+      this.asqJavaq = new this.asqJavaqPlugin(this.asq);
+      this.asJavaqPlugin.prototype.processEl.reset();
       this.create.reset();
     });
 
     after(function(){
-     this.asqMcPlugin.prototype.processEl.restore();
+     this.asqJavaqPlugin.prototype.processEl.restore();
     });
 
-    it("should call processEl() for all asq-multi-choice-q elements", function(done){
-      this.asqmc.parseHtml(this.simpleHtml)
+    it("should call processEl() for all asq-java-q elements", function(done){
+      this.asqJavaq.parseHtml(this.simpleHtml)
       .then(function(){
-        this.asqmc.processEl.calledTwice.should.equal(true);
+        this.asqJavaq.processEl.calledTwice.should.equal(true);
         done();
       }.bind(this))
       .catch(function(err){
@@ -69,7 +69,7 @@ describe("asqMcPlugin.js", function(){
     });
 
     it("should call `model().create()` to persist parsed questions in the db", function(done){
-      this.asqmc.parseHtml(this.simpleHtml)
+      this.asqJavaq.parseHtml(this.simpleHtml)
       .then(function(result){
         this.create.calledOnce.should.equal(true);
         this.create.calledWith(["res", "res"]).should.equal(true);
@@ -81,7 +81,7 @@ describe("asqMcPlugin.js", function(){
     });
 
     it("should resolve with the file's html", function(done){
-      this.asqmc.parseHtml(this.simpleHtml)
+      this.asqJavaq.parseHtml(this.simpleHtml)
       .then(function(result){
         expect(result).to.equal(this.simpleHtml);
         done();
@@ -96,30 +96,30 @@ describe("asqMcPlugin.js", function(){
   describe("processEl", function(){
 
     before(function(){
-     sinon.stub(this.asqMcPlugin.prototype, "parseOptions").returns([]);
+     sinon.stub(this.asqJavaqPlugin.prototype, "parseOptions").returns([]);
     });
 
     beforeEach(function(){
-      this.asqmc = new this.asqMcPlugin(this.asq);
-      this.asqMcPlugin.prototype.parseOptions.reset();
+      this.asqJavaq = new this.asqJavaqPlugin(this.asq);
+      this.asqJavaqPlugin.prototype.parseOptions.reset();
     });
 
     after(function(){
-     this.asqMcPlugin.prototype.parseOptions.restore();
+     this.asqJavaqPlugin.prototype.parseOptions.restore();
     });
 
     it("should assign a uid to the question if there's not one", function(){
       var $ = cheerio.load(this.simpleHtml);
-      
+
       //this doesn't have an id
       var el = $("#no-uid")[0];
-      this.asqmc.processEl($, el);
+      this.asqJavaq.processEl($, el);
       $(el).attr('uid').should.exist;
       $(el).attr('uid').should.not.equal("a-uid");
 
       //this already has one
       el = $("#uid")[0];
-      this.asqmc.processEl($, el);
+      this.asqJavaq.processEl($, el);
       $(el).attr('uid').should.exist;
       $(el).attr('uid').should.equal("a-uid");
     });
@@ -128,8 +128,8 @@ describe("asqMcPlugin.js", function(){
       var $ = cheerio.load(this.simpleHtml);
       var el = $(this.tagName)[0];
 
-      this.asqmc.processEl($, el);
-      this.asqmc.parseOptions.calledOnce.should.equal(true);
+      this.asqJavaq.processEl($, el);
+      this.asqJavaq.parseOptions.calledOnce.should.equal(true);
     });
 
     it("should find the stem if it exists", function(){
@@ -137,16 +137,16 @@ describe("asqMcPlugin.js", function(){
       var el = $(this.tagName)[0];
       var elWithHtmlInStem = $(this.tagName)[1];
 
-      var result = this.asqmc.processEl($, el);
+      var result = this.asqJavaq.processEl($, el);
       expect(result.data.stem).to.equal("This is a stem");
 
-      var result = this.asqmc.processEl($, elWithHtmlInStem);
+      var result = this.asqJavaq.processEl($, elWithHtmlInStem);
       expect(result.data.stem).to.equal("This is a stem <em>with some HTML</em>");
 
 
       var $ = cheerio.load(this.noStemHtml);
       var el = $(this.tagName)[0];
-      var result = this.asqmc.processEl($, el);
+      var result = this.asqJavaq.processEl($, el);
       expect(result.data.stem).to.equal("");
     });
 
@@ -154,7 +154,7 @@ describe("asqMcPlugin.js", function(){
       var $ = cheerio.load(this.simpleHtml);
       var el = $(this.tagName)[1];
 
-      var result = this.asqmc.processEl($, el);
+      var result = this.asqJavaq.processEl($, el);
       expect(result._id).to.equal("a-uid");
       expect(result.type).to.equal(this.tagName);
       expect(result.data.stem).to.equal("This is a stem <em>with some HTML</em>");
@@ -166,16 +166,16 @@ describe("asqMcPlugin.js", function(){
 
     beforeEach(function(){
       this.$ = cheerio.load(this.optionsHtml);
-      this.asqmc = new this.asqMcPlugin(this.asq);
+      this.asqJavaq = new this.asqJavaqPlugin(this.asq);
     });
 
     it("should throw an error when there less than two `asq-option` tags", function(){
       var el = this.$("#no-options")[0];
-      var bindedFn = this.asqmc.parseOptions.bind(this.asqmc, this.$, el);
+      var bindedFn = this.asqJavaq.parseOptions.bind(this.asqJavaq, this.$, el);
       expect(bindedFn).to.throw(/at least two `asq-option` children/);
 
       el = this.$("#one-option")[0];
-      bindedFn = this.asqmc.parseOptions.bind(this.asqmc, this.$, el);
+      bindedFn = this.asqJavaq.parseOptions.bind(this.asqJavaq, this.$, el);
       expect(bindedFn).to.throw(/at least two `asq-option` children/);
     });
 
@@ -183,13 +183,13 @@ describe("asqMcPlugin.js", function(){
       var el;
 
       el = this.$("#no-uids")[0];
-      this.asqmc.parseOptions(this.$, el);
+      this.asqJavaq.parseOptions(this.$, el);
       this.$(el).find('asq-option').eq(0).attr('uid').should.exist;
       this.$(el).find('asq-option').eq(1).attr('uid').should.exist;
       this.$(el).find('asq-option').eq(2).attr('uid').should.exist;
 
       el = this.$("#uids-ok")[0];
-      this.asqmc.parseOptions(this.$, el);
+      this.asqJavaq.parseOptions(this.$, el);
       this.$(el).find('asq-option').eq(0).attr('uid').should.equal("0123456789abcd0123456781");
       this.$(el).find('asq-option').eq(1).attr('uid').should.equal("0123456789abcd0123456782");
       this.$(el).find('asq-option').eq(2).attr('uid').should.equal("0123456789abcd0123456783");
@@ -197,13 +197,13 @@ describe("asqMcPlugin.js", function(){
 
     it("should throw an error when there are more than one options with the same uid", function(){
       var el = this.$("#same-uids")[0];
-      var bindedFn = this.asqmc.parseOptions.bind(this.asqmc, this.$, el);
+      var bindedFn = this.asqJavaq.parseOptions.bind(this.asqJavaq, this.$, el);
       expect(bindedFn).to.throw(/cannot have two `asq-option` elements with the same uids/);
     });
 
     it("should parse the `correct` attribute of options", function(){
       var el = this.$("#correct")[0];
-      var result = this.asqmc.parseOptions(this.$, el);
+      var result = this.asqJavaq.parseOptions(this.$, el);
       expect(result[0].correct).to.equal(false);
       expect(result[1].correct).to.equal(true);
       expect(result[2].correct).to.equal(true);
@@ -211,7 +211,7 @@ describe("asqMcPlugin.js", function(){
 
     it("should output the correct data", function(){
       var el = this.$("#correct")[0];
-      var result = this.asqmc.parseOptions(this.$, el);
+      var result = this.asqJavaq.parseOptions(this.$, el);
       expect(result[0]._id.toString()).to.equal("0123456789abcd0123456781");
       expect(result[0].html).to.equal("Option 1");
       expect(result[0].correct).to.equal(false);

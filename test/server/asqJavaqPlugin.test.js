42b57b57a4f9416d648facc5e9972dd8f684b59f
Kyriakos Stylianopoulos <usi@usis-mbp.mobile.usilu.net>
2016-12-07 15:47:18 +0100

    minor change to parseHtml->test code

--- test/server/asqJavaqPlugin.test.js
+++ test/server/asqJavaqPlugin.test.js
@@ -36,7 +36,7 @@ describe("asqJavaqPlugin.js", function(){
     //load html fixtures
     this.simpleHtml = fs.readFileSync(require.resolve('./fixtures/simple.html'), 'utf-8');
     this.noStemHtml = fs.readFileSync(require.resolve('./fixtures/no-stem.html'), 'utf-8');
-    this.optionsHtml = fs.readFileSync(require.resolve('./fixtures/options.html'), 'utf-8');
+    //this.optionsHtml = fs.readFileSync(require.resolve('./fixtures/options.html'), 'utf-8');
 
     this.asqJavaqPlugin = require(modulePath);
   });
@@ -97,17 +97,11 @@ describe("asqJavaqPlugin.js", function(){
 
   describe.skip("processEl", function(){
 
-    before(function(){
-     sinon.stub(this.asqJavaqPlugin.prototype, "parseOptions").returns([]);
-    });
-
     beforeEach(function(){
       this.asqJavaq = new this.asqJavaqPlugin(this.asq);
-      this.asqJavaqPlugin.prototype.parseOptions.reset();
     });
 
     after(function(){
-     this.asqJavaqPlugin.prototype.parseOptions.restore();
     });
 
     it("should assign a uid to the question if there's not one", function(){
@@ -126,13 +120,6 @@ describe("asqJavaqPlugin.js", function(){
       $(el).attr('uid').should.equal("a-uid");
     });
 
-    it("should call parseOptions()", function(){
-      var $ = cheerio.load(this.simpleHtml);
-      var el = $(this.tagName)[0];
-
-      this.asqJavaq.processEl($, el);
-      this.asqJavaq.parseOptions.calledOnce.should.equal(true);
-    });
 
     it("should find the stem if it exists", function(){
       var $ = cheerio.load(this.simpleHtml);
@@ -160,69 +147,9 @@ describe("asqJavaqPlugin.js", function(){
       expect(result._id).to.equal("a-uid");
       expect(result.type).to.equal(this.tagName);
       expect(result.data.stem).to.equal("This is a stem <em>with some HTML</em>");
-      expect(result.data.options).to.deep.equal([]);
+      //expect(result.data.options).to.deep.equal([]);
     });
   });
 
-  describe.skip("parseOptions", function(){
 
-    beforeEach(function(){
-      this.$ = cheerio.load(this.optionsHtml);
-      this.asqJavaq = new this.asqJavaqPlugin(this.asq);
-    });
-
-    it("should throw an error when there less than two `asq-option` tags", function(){
-      var el = this.$("#no-options")[0];
-      var bindedFn = this.asqJavaq.parseOptions.bind(this.asqJavaq, this.$, el);
-      expect(bindedFn).to.throw(/at least two `asq-option` children/);
-
-      el = this.$("#one-option")[0];
-      bindedFn = this.asqJavaq.parseOptions.bind(this.asqJavaq, this.$, el);
-      expect(bindedFn).to.throw(/at least two `asq-option` children/);
-    });
-
-    it("should assign a uid to options that don't have one", function(){
-      var el;
-
-      el = this.$("#no-uids")[0];
-      this.asqJavaq.parseOptions(this.$, el);
-      this.$(el).find('asq-option').eq(0).attr('uid').should.exist;
-      this.$(el).find('asq-option').eq(1).attr('uid').should.exist;
-      this.$(el).find('asq-option').eq(2).attr('uid').should.exist;
-
-      el = this.$("#uids-ok")[0];
-      this.asqJavaq.parseOptions(this.$, el);
-      this.$(el).find('asq-option').eq(0).attr('uid').should.equal("0123456789abcd0123456781");
-      this.$(el).find('asq-option').eq(1).attr('uid').should.equal("0123456789abcd0123456782");
-      this.$(el).find('asq-option').eq(2).attr('uid').should.equal("0123456789abcd0123456783");
-    });
-
-    it("should throw an error when there are more than one options with the same uid", function(){
-      var el = this.$("#same-uids")[0];
-      var bindedFn = this.asqJavaq.parseOptions.bind(this.asqJavaq, this.$, el);
-      expect(bindedFn).to.throw(/cannot have two `asq-option` elements with the same uids/);
-    });
-
-    it("should parse the `correct` attribute of options", function(){
-      var el = this.$("#correct")[0];
-      var result = this.asqJavaq.parseOptions(this.$, el);
-      expect(result[0].correct).to.equal(false);
-      expect(result[1].correct).to.equal(true);
-      expect(result[2].correct).to.equal(true);
-    });
-
-    it("should output the correct data", function(){
-      var el = this.$("#correct")[0];
-      var result = this.asqJavaq.parseOptions(this.$, el);
-      expect(result[0]._id.toString()).to.equal("0123456789abcd0123456781");
-      expect(result[0].html).to.equal("Option 1");
-      expect(result[0].correct).to.equal(false);
-      expect(result[1]._id.toString()).to.equal("0123456789abcd0123456782");
-      expect(result[1].html).to.equal("Option <em>2</em>");
-      expect(result[1].correct).to.equal(true);
-      expect(result[2]._id.toString()).to.equal("0123456789abcd0123456783");
-      expect(result[2].html).to.equal("Option 3");
-      expect(result[2].correct).to.equal(true);
-    });
-  });
 });

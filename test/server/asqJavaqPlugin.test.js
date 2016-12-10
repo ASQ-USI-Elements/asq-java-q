ea7c1d4cfc567862a0cb91f9459e87c96283263c
Patrick Balestra <me@patrickbalestra.com>
2016-12-10 16:35:57 +0100

    Squashed commit of the following:
    commit d255bf756881a1ebbe7d928f227d09d3b3c4d379
    Author: Marco Tollini <marco.mt19@gmail.com>
    Date:   Sat Dec 10 15:54:57 2016 +0100
    
        Fixed indentation
    
    commit e9e522711b62b0bfa7f312cf876ee7352c529d36
    Merge: 1ad16f8 80afc14
    Author: Marco Tollini <marco.mt19@gmail.com>
    Date:   Sat Dec 10 15:39:21 2016 +0100
    
        Merged branch communication into communication
    
    commit 80afc1491084269289026937663736260200f486
    Merge: 9e6ee29 e709ddf
    Author: Kyriakos Stylianopoulos <usi@usis-mbp.mobile.usilu.net>
    Date:   Sat Dec 10 15:38:38 2016 +0100
    
        Merge branch 'communication' of https://github.com/ASQ-USI-Elements/asq-java-q into communication
    
    commit 9e6ee29997826c1f05d81fd461a4bf4711e241fa
    Author: Kyriakos Stylianopoulos <usi@usis-mbp.mobile.usilu.net>
    Date:   Sat Dec 10 15:38:22 2016 +0100
    
        encapsulated communicating function to server to an asynchronous
    
    commit 1ad16f8ec3448c8e6d4e6a431becaa62646a663d
    Merge: e709ddf 3723721
    Author: Marco Tollini <marco.mt19@gmail.com>
    Date:   Sat Dec 10 15:19:09 2016 +0100
    
        Merge branch 'backend_test' into communication
    
    commit 372372195537546ce057f660808a50559d0d9b90
    Author: Marco Tollini <marco.mt19@gmail.com>
    Date:   Sat Dec 10 15:13:42 2016 +0100
    
        Working tests
    
    commit e709ddfdd2bec0ad4c604819632871accf9e8338
    Author: Vasileios Triglianos <triglian@gmail.com>
    Date:   Sat Dec 10 14:57:26 2016 +0100
    
        update travis secrets and add correct testing commands
    
    commit be9c6894c5d1a1d4856489148a803d912f7a980f
    Author: Marco Tollini <marco.mt19@gmail.com>
    Date:   Sat Dec 10 14:46:40 2016 +0100
    
        Fixed some tests
    
    commit fd11f45f3e9fb5c3f1863061513fff775bbe2d2b
    Author: Patrick Balestra <me@patrickbalestra.com>
    Date:   Thu Dec 8 17:00:50 2016 +0100
    
        Add travis.yml and modify gitignore
    
    commit 42b57b57a4f9416d648facc5e9972dd8f684b59f
    Author: Kyriakos Stylianopoulos <usi@usis-mbp.mobile.usilu.net>
    Date:   Wed Dec 7 15:47:18 2016 +0100
    
        minor change to parseHtml->test code

--- test/server/asqJavaqPlugin.test.js
+++ test/server/asqJavaqPlugin.test.js
@@ -24,6 +24,7 @@ describe("asqJavaqPlugin.js", function(){
 
     this.asq = {
       registerHook: function(){},
+      registerEvent: function(){},
       db: {
         model: function(){
           return {
@@ -36,7 +37,7 @@ describe("asqJavaqPlugin.js", function(){
     //load html fixtures
     this.simpleHtml = fs.readFileSync(require.resolve('./fixtures/simple.html'), 'utf-8');
     this.noStemHtml = fs.readFileSync(require.resolve('./fixtures/no-stem.html'), 'utf-8');
-    this.optionsHtml = fs.readFileSync(require.resolve('./fixtures/options.html'), 'utf-8');
+    //this.optionsHtml = fs.readFileSync(require.resolve('./fixtures/options.html'), 'utf-8');
 
     this.asqJavaqPlugin = require(modulePath);
   });
@@ -70,8 +71,10 @@ describe("asqJavaqPlugin.js", function(){
       })
     });
 
-    it.skip("should call `model().create()` to persist parsed questions in the db", function(done){
-      this.asqJavaq.parseHtml(this.simpleHtml)
+    it("should call `model().create()` to persist parsed questions in the db", function(done){
+      this.asqJavaq.parseHtml({
+        html: this.simpleHtml
+      })
       .then(function(result){
         this.create.calledOnce.should.equal(true);
         this.create.calledWith(["res", "res"]).should.equal(true);
@@ -82,10 +85,12 @@ describe("asqJavaqPlugin.js", function(){
       })
     });
 
-    it.skip("should resolve with the file's html", function(done){
-      this.asqJavaq.parseHtml(this.simpleHtml)
+    it("should resolve with the file's html", function(done){
+      this.asqJavaq.parseHtml({
+        html: this.simpleHtml
+      })
       .then(function(result){
-        expect(result).to.equal(this.simpleHtml);
+        expect(result.html).to.equal(this.simpleHtml);
         done();
       }.bind(this))
       .catch(function(err){
@@ -95,19 +100,15 @@ describe("asqJavaqPlugin.js", function(){
 
   });
 
-  describe.skip("processEl", function(){
+  describe("processEl", function(){
 
     before(function(){
-     sinon.stub(this.asqJavaqPlugin.prototype, "parseOptions").returns([]);
+     sinon.stub(this.asqJavaqPlugin.prototype, "parseSettings").returns(Promise.resolve({}));
+
     });
 
     beforeEach(function(){
       this.asqJavaq = new this.asqJavaqPlugin(this.asq);
-      this.asqJavaqPlugin.prototype.parseOptions.reset();
-    });
-
-    after(function(){
-     this.asqJavaqPlugin.prototype.parseOptions.restore();
     });
 
     it("should assign a uid to the question if there's not one", function(){
@@ -126,103 +127,57 @@ describe("asqJavaqPlugin.js", function(){
       $(el).attr('uid').should.equal("a-uid");
     });
 
-    it("should call parseOptions()", function(){
-      var $ = cheerio.load(this.simpleHtml);
-      var el = $(this.tagName)[0];
 
-      this.asqJavaq.processEl($, el);
-      this.asqJavaq.parseOptions.calledOnce.should.equal(true);
-    });
-
-    it("should find the stem if it exists", function(){
+    it("should find the stem if it exists", function(done){
       var $ = cheerio.load(this.simpleHtml);
       var el = $(this.tagName)[0];
       var elWithHtmlInStem = $(this.tagName)[1];
 
       var result = this.asqJavaq.processEl($, el);
-      expect(result.data.stem).to.equal("This is a stem");
+      result.then(function(result){
+        expect(result.data.stem).to.equal("This is a stem");
+      }.bind(this))
+      .catch(function(err){
+        done(err);
+      });
 
       var result = this.asqJavaq.processEl($, elWithHtmlInStem);
-      expect(result.data.stem).to.equal("This is a stem <em>with some HTML</em>");
-
+      result.then(function(result){
+        expect(result.data.stem).to.equal("This is a stem <em>with some HTML</em>");
+      }.bind(this))
+      .catch(function(err){
+        done(err);
+      });
 
       var $ = cheerio.load(this.noStemHtml);
       var el = $(this.tagName)[0];
       var result = this.asqJavaq.processEl($, el);
-      expect(result.data.stem).to.equal("");
+      result.then(function(result){
+        expect(result.data.stem).to.equal("");
+        done();
+      }.bind(this))
+      .catch(function(err){
+        done(err);
+      });
     });
 
-    it("should return correct data", function(){
+    it("should return correct data", function(done){
       var $ = cheerio.load(this.simpleHtml);
       var el = $(this.tagName)[1];
 
       var result = this.asqJavaq.processEl($, el);
-      expect(result._id).to.equal("a-uid");
-      expect(result.type).to.equal(this.tagName);
-      expect(result.data.stem).to.equal("This is a stem <em>with some HTML</em>");
-      expect(result.data.options).to.deep.equal([]);
+      result.then(function(result){
+        expect(result._id).to.equal("a-uid");
+        expect(result.type).to.equal(this.tagName);
+        expect(result.data.stem).to.equal("This is a stem <em>with some HTML</em>");
+
+        done();
+      }.bind(this))
+      .catch(function(err){
+        done(err);
+      });
     });
   });
 
-  describe.skip("parseOptions", function(){
 
-    beforeEach(function(){
-      this.$ = cheerio.load(this.optionsHtml);
-      this.asqJavaq = new this.asqJavaqPlugin(this.asq);
-    });
-
-    it("should throw an error when there less than two `asq-option` tags", function(){
-      var el = this.$("#no-options")[0];
-      var bindedFn = this.asqJavaq.parseOptions.bind(this.asqJavaq, this.$, el);
-      expect(bindedFn).to.throw(/at least two `asq-option` children/);
-
-      el = this.$("#one-option")[0];
-      bindedFn = this.asqJavaq.parseOptions.bind(this.asqJavaq, this.$, el);
-      expect(bindedFn).to.throw(/at least two `asq-option` children/);
-    });
-
-    it("should assign a uid to options that don't have one", function(){
-      var el;
-
-      el = this.$("#no-uids")[0];
-      this.asqJavaq.parseOptions(this.$, el);
-      this.$(el).find('asq-option').eq(0).attr('uid').should.exist;
-      this.$(el).find('asq-option').eq(1).attr('uid').should.exist;
-      this.$(el).find('asq-option').eq(2).attr('uid').should.exist;
-
-      el = this.$("#uids-ok")[0];
-      this.asqJavaq.parseOptions(this.$, el);
-      this.$(el).find('asq-option').eq(0).attr('uid').should.equal("0123456789abcd0123456781");
-      this.$(el).find('asq-option').eq(1).attr('uid').should.equal("0123456789abcd0123456782");
-      this.$(el).find('asq-option').eq(2).attr('uid').should.equal("0123456789abcd0123456783");
-    });
-
-    it("should throw an error when there are more than one options with the same uid", function(){
-      var el = this.$("#same-uids")[0];
-      var bindedFn = this.asqJavaq.parseOptions.bind(this.asqJavaq, this.$, el);
-      expect(bindedFn).to.throw(/cannot have two `asq-option` elements with the same uids/);
-    });
-
-    it("should parse the `correct` attribute of options", function(){
-      var el = this.$("#correct")[0];
-      var result = this.asqJavaq.parseOptions(this.$, el);
-      expect(result[0].correct).to.equal(false);
-      expect(result[1].correct).to.equal(true);
-      expect(result[2].correct).to.equal(true);
-    });
-
-    it("should output the correct data", function(){
-      var el = this.$("#correct")[0];
-      var result = this.asqJavaq.parseOptions(this.$, el);
-      expect(result[0]._id.toString()).to.equal("0123456789abcd0123456781");
-      expect(result[0].html).to.equal("Option 1");
-      expect(result[0].correct).to.equal(false);
-      expect(result[1]._id.toString()).to.equal("0123456789abcd0123456782");
-      expect(result[1].html).to.equal("Option <em>2</em>");
-      expect(result[1].correct).to.equal(true);
-      expect(result[2]._id.toString()).to.equal("0123456789abcd0123456783");
-      expect(result[2].html).to.equal("Option 3");
-      expect(result[2].correct).to.equal(true);
-    });
-  });
 });
